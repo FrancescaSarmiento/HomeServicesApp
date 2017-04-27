e299072a2f0988efff2f9689f13f4dc4fa6cb990
Jerome del Rosario <jeromed.pepper@gmail.com>
2017-04-27 20:39:20 +0800

    Upadated handyzebdb database

--- handyzebdb.sql
+++ handyzebdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 26, 2017 at 02:30 AM
+-- Generation Time: Apr 27, 2017 at 12:38 PM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
@@ -31,19 +31,27 @@ CREATE TABLE `customer` (
   `firstName` varchar(30) NOT NULL,
   `lastName` varchar(30) NOT NULL,
   `userName` varchar(15) NOT NULL,
-  `pasword` varchar(20) NOT NULL,
-  `address` varchar(30) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `address` varchar(50) NOT NULL,
   `email` varchar(30) DEFAULT NULL,
-  `contactNumber` varchar(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `contactNumber` varchar(11) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `customer`
 --
 
-INSERT INTO `customer` (`customerNumber`, `firstName`, `lastName`, `userName`, `pasword`, `address`, `email`, `contactNumber`) VALUES
-(1, 'Jerome', 'Del Rosario', 'Muzhnik', 'password', 'City Camp Alley, Baguio City', 'jeromed.pepper@gmail.com', '09153478922'),
-(2, 'Dadula', 'Jason Paul', 'Jay', 'password', 'Baguio City', NULL, NULL);
+INSERT INTO `customer` (`customerNumber`, `firstName`, `lastName`, `userName`, `password`, `address`, `email`, `contactNumber`, `status`) VALUES
+(1, 'Jerome', 'Del Rosario', 'Muzhnik', 'password', 'City Camp Alley, Baguio City', 'jeromed.pepper@gmail.com', '09153478922', 1),
+(2, 'Jay', 'Dadula', 'Jay', 'password', 'Baguio City', NULL, '09123456789', 1),
+(3, 'Michael', 'Rivera', 'Shiru', 'password', 'Baguio City', NULL, '09098765432', 1),
+(4, 'Rio Vann', 'Kolodzik', 'MVLE', 'password', 'Baguio City', NULL, '09090909090', 1),
+(5, 'Josepablo', 'David', 'PPJJ', 'password', 'Baguio City', NULL, '09154893264', 1),
+(6, 'David Wyatt', 'Cardenas', 'Moo Man', 'password', 'Baguio City', NULL, '09053216547', 1),
+(7, 'Hiromi', 'Uematsu', 'Japan420', 'password', 'Ching Chong, Japan', '', '09156669999', 1),
+(8, 'Samantha', 'Lopez', 'samiam', 'password', 'Baguio City', NULL, '09124563219', 1),
+(9, 'Miggy', 'Sarmiento', 'Miggy2321241', 'password', 'Baguio City', NULL, '09159874561', 1);
 
 -- --------------------------------------------------------
 
@@ -56,8 +64,8 @@ CREATE TABLE `message` (
   `messageBody` text NOT NULL,
   `customerNumber` int(11) NOT NULL,
   `spNumber` int(11) NOT NULL,
-  `sendTo` enum('customer','sp') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `sendTo` enum('cust','sp') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -67,20 +75,21 @@ CREATE TABLE `message` (
 
 CREATE TABLE `service` (
   `serviceNumber` int(11) NOT NULL,
-  `serviceType` enum('Repair','Assembly/Installation','Electrical','Painting','Plumbing') NOT NULL,
-  `specification` text NOT NULL,
-  `Price` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `serviceType` enum('Assembly/Installation','Repair','Plumbing','Electrical','Painting','Carpentry','Remodeling') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `service`
 --
 
-INSERT INTO `service` (`serviceNumber`, `serviceType`, `specification`, `Price`) VALUES
-(1, 'Repair', 'Roof repairs', 500),
-(2, 'Plumbing', 'Toilet', 750),
-(3, 'Assembly/Installation', 'ikea table', 500),
-(4, 'Electrical', 'rewiring', 550);
+INSERT INTO `service` (`serviceNumber`, `serviceType`) VALUES
+(1, 'Assembly/Installation'),
+(2, 'Repair'),
+(3, 'Plumbing'),
+(4, 'Electrical'),
+(5, 'Painting'),
+(6, 'Carpentry'),
+(7, 'Remodeling');
 
 -- --------------------------------------------------------
 
@@ -94,17 +103,26 @@ CREATE TABLE `service_provider` (
   `lastName` varchar(30) NOT NULL,
   `userName` varchar(15) NOT NULL,
   `password` varchar(20) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `contactNumber` varchar(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `email` varchar(30) DEFAULT NULL,
+  `contactNumber` varchar(11) NOT NULL,
+  `availability` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `service_provider`
 --
 
-INSERT INTO `service_provider` (`spNumber`, `firstName`, `lastName`, `userName`, `password`, `email`, `contactNumber`) VALUES
-(1, 'Lexington', 'Steele', 'bigdaddylex23', 'password', 'lexington@hotmail.com', '09123456789'),
-(2, 'Zebedee', 'Jimenez', 'bhosx_zeb', 'password', '2150936@slu.edu.ph', '09123456789');
+INSERT INTO `service_provider` (`spNumber`, `firstName`, `lastName`, `userName`, `password`, `email`, `contactNumber`, `availability`) VALUES
+(1, 'James', 'Deen', 'jaaaaaymes', 'password', NULL, '1', 1),
+(2, 'Mick', 'Blue', 'blubols29', 'password', NULL, '5', 1),
+(3, 'Mr', 'Pete', 'asspire', 'password', NULL, '545', 1),
+(4, 'Mr', 'Marcus', 'marcusindahouse', 'password', NULL, '6549', 1),
+(5, 'Peter', 'North', 'pyter', 'password', NULL, '69', 1),
+(6, 'Evan', 'Stone', 'mercurydrug', 'password', NULL, '89', 1),
+(7, 'Rocco', 'Siffredi', 'RoccoLocco', 'password', NULL, '465987', 1),
+(8, 'Lexington', 'Steele', 'daddysteele', 'password', NULL, '321', 1),
+(9, 'Zebedee', 'Jimenez', 'bhosx_zeb', 'password', NULL, '78985612', 1),
+(10, 'Johnny', 'Sins', 'heresjohny61', 'password', NULL, '09991234567', 1);
 
 -- --------------------------------------------------------
 
@@ -115,7 +133,7 @@ INSERT INTO `service_provider` (`spNumber`, `firstName`, `lastName`, `userName`,
 CREATE TABLE `sp_service` (
   `spNumber` int(11) NOT NULL,
   `serviceNumber` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `sp_service`
@@ -123,9 +141,34 @@ CREATE TABLE `sp_service` (
 
 INSERT INTO `sp_service` (`spNumber`, `serviceNumber`) VALUES
 (1, 1),
-(1, 2),
+(1, 3),
+(1, 4),
+(1, 7),
+(2, 5),
 (2, 1),
-(2, 4);
+(2, 4),
+(3, 6),
+(4, 5),
+(4, 4),
+(5, 7),
+(5, 1),
+(5, 2),
+(6, 6),
+(6, 1),
+(6, 3),
+(7, 2),
+(7, 1),
+(7, 3),
+(7, 4),
+(7, 5),
+(7, 6),
+(7, 7),
+(8, 2),
+(8, 1),
+(8, 4),
+(9, 2),
+(9, 5),
+(9, 4);
 
 -- --------------------------------------------------------
 
@@ -136,11 +179,13 @@ INSERT INTO `sp_service` (`spNumber`, `serviceNumber`) VALUES
 CREATE TABLE `transaction` (
   `transactionNumber` int(11) NOT NULL,
   `customerNumber` int(11) NOT NULL,
-  `spNumber` int(11) NOT NULL,
+  `spNumber` int(11) DEFAULT NULL,
   `serviceNumber` int(11) NOT NULL,
-  `date_started` date NOT NULL,
-  `date_finished` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `specification` text NOT NULL,
+  `status` enum('Request Pending','Ongoing','Finished') NOT NULL,
+  `date_started` timestamp NOT NULL,
+  `date_finished` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Indexes for dumped tables
@@ -151,7 +196,8 @@ CREATE TABLE `transaction` (
 --
 ALTER TABLE `customer`
   ADD PRIMARY KEY (`customerNumber`),
-  ADD UNIQUE KEY `customerNumber` (`customerNumber`,`userName`,`email`);
+  ADD UNIQUE KEY `USERNAME` (`userName`),
+  ADD UNIQUE KEY `CONTACTNUMBER` (`contactNumber`);
 
 --
 -- Indexes for table `message`
@@ -165,14 +211,16 @@ ALTER TABLE `message`
 -- Indexes for table `service`
 --
 ALTER TABLE `service`
-  ADD PRIMARY KEY (`serviceNumber`);
+  ADD PRIMARY KEY (`serviceNumber`),
+  ADD UNIQUE KEY `TYPE` (`serviceType`);
 
 --
 -- Indexes for table `service_provider`
 --
 ALTER TABLE `service_provider`
   ADD PRIMARY KEY (`spNumber`),
-  ADD UNIQUE KEY `spNumber` (`spNumber`,`userName`,`email`);
+  ADD UNIQUE KEY `USERNAME` (`userName`),
+  ADD UNIQUE KEY `CONTACTNUMBER` (`contactNumber`);
 
 --
 -- Indexes for table `sp_service`
@@ -191,6 +239,35 @@ ALTER TABLE `transaction`
   ADD KEY `serviceNumber` (`serviceNumber`);
 
 --
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `customerNumber` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT for table `message`
+--
+ALTER TABLE `message`
+  MODIFY `messageNumber` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `service`
+--
+ALTER TABLE `service`
+  MODIFY `serviceNumber` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `service_provider`
+--
+ALTER TABLE `service_provider`
+  MODIFY `spNumber` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `transactionNumber` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- Constraints for dumped tables
 --
 
@@ -198,23 +275,23 @@ ALTER TABLE `transaction`
 -- Constraints for table `message`
 --
 ALTER TABLE `message`
-  ADD CONSTRAINT `customer_message_fk` FOREIGN KEY (`customerNumber`) REFERENCES `customer` (`customerNumber`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `sp_message_fk` FOREIGN KEY (`spNumber`) REFERENCES `service_provider` (`spNumber`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `message_customer_fk` FOREIGN KEY (`customerNumber`) REFERENCES `customer` (`customerNumber`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `message_sp_fk` FOREIGN KEY (`spNumber`) REFERENCES `service_provider` (`spNumber`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `sp_service`
 --
 ALTER TABLE `sp_service`
-  ADD CONSTRAINT `service_fk` FOREIGN KEY (`serviceNumber`) REFERENCES `service` (`serviceNumber`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `sp_fk` FOREIGN KEY (`spNumber`) REFERENCES `service_provider` (`spNumber`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `spserv_service_fk` FOREIGN KEY (`serviceNumber`) REFERENCES `service` (`serviceNumber`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `spserv_sp_fk` FOREIGN KEY (`spNumber`) REFERENCES `service_provider` (`spNumber`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `transaction`
 --
 ALTER TABLE `transaction`
-  ADD CONSTRAINT `customer_transaction_fk` FOREIGN KEY (`customerNumber`) REFERENCES `customer` (`customerNumber`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `service_transaction_fk` FOREIGN KEY (`serviceNumber`) REFERENCES `service` (`serviceNumber`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `sp_transaction_fk` FOREIGN KEY (`spNumber`) REFERENCES `service_provider` (`spNumber`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `transaction_cust_fk` FOREIGN KEY (`customerNumber`) REFERENCES `customer` (`customerNumber`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `transaction_service_fk` FOREIGN KEY (`serviceNumber`) REFERENCES `service` (`serviceNumber`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `transaction_sp_fk` FOREIGN KEY (`spNumber`) REFERENCES `service_provider` (`spNumber`) ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
