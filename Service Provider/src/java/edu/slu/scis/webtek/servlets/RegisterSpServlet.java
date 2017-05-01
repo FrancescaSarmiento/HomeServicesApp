f46560818724e8c1420a3c264596d04477f191da
Jerome del Rosario <jeromed.pepper@gmail.com>
2017-05-01 20:22:25 +0800

    Added fixed project files

--- Service Provider/src/java/edu/slu/scis/webtek/servlets/RegisterSpServlet.java
+++ Service Provider/src/java/edu/slu/scis/webtek/servlets/RegisterSpServlet.java
@@ -1,6 +1,7 @@
-package servlets;
+package edu.slu.scis.webtek.servlets;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -13,6 +14,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 @WebServlet(name = "RegisterSpServlet", urlPatterns = {"/RegisterSpServlet"})
 @MultipartConfig
@@ -22,36 +24,30 @@ public class RegisterSpServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
-        String firstName = request.getParameter("first");
-        String lastName = request.getParameter("last");
-        String userName = request.getParameter("user");
-        String password = request.getParameter("pass");
-        String email = request.getParameter("mail");
-        String contact = request.getParameter("contact");
-
+        String id = request.getParameter("id");
+        String desc = request.getParameter("desc");
+        BigDecimal price = new BigDecimal(request.getParameter("price"));
+        Part image = request.getPart("image");
+        
         try {
             Class.forName("com.mysql.jdbc.Driver");
             
             String connUrl = "jdbc:mysql://localhost:3306/handyzebdb?user=root&password=";
             Connection conn = DriverManager.getConnection(connUrl);
             
-            String sql = "INSERT INTO `service_provider` (`spNumber`, `firstName`, `lastName`, `userName`, `password`, `email`, `contactNumber`, `availability`, `status`) VALUES (NULL, ?, ?, ?, ?, ?, ?, '1', '0')";
+            String sql = "INSERT INTO products(prodid, proddesc, price) VALUES (?, ?, ?)";
             PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setString(1, firstName);
-            ps.setString(2, lastName);
-            ps.setString(3, userName);
-            ps.setString(4, password);
-            ps.setString(5, email);
-            ps.setString(6, contact);
+            ps.setString(1, id);
+            ps.setString(2, desc);
+            ps.setBigDecimal(3, price);
             
             boolean success = true;
             try {
                 ps.executeUpdate();
 
-                /*ServletContext context = this.getServletContext();
+                ServletContext context = this.getServletContext();
                 String imagePath = context.getRealPath("/productimages/") + id + ".jpg";
                 image.write(imagePath);
-                */
             } catch (Exception e) {
                 Logger.getLogger(RegisterSpServlet.class.getName()).log(Level.SEVERE, null, e);
                 success = false;
