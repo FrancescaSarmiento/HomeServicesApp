6210c3e6d5b998332762be1218a41fb544fb7223
smglopez <2158841@slu.edu.ph>
2017-05-09 16:56:51 +0800

    latest db

--- handyzebdb.sql
+++ handyzebdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 08, 2017 at 01:49 PM
+-- Generation Time: May 09, 2017 at 08:56 AM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
@@ -63,7 +63,11 @@ CREATE TABLE `booking` (
 --
 
 INSERT INTO `booking` (`bookingId`, `custId`, `spId`, `serviceId`, `bookingStatus`, `reserved_date`, `dateStarted`, `dateFinished`) VALUES
-(3, 7, 12, 3, 'pending', '2017-05-08 10:00:00', NULL, NULL);
+(3, 7, 11, 3, 'pending', '2017-05-12 10:00:00', NULL, NULL),
+(4, 7, 12, 4, 'pending', '2017-05-10 10:00:00', NULL, NULL),
+(5, 7, 12, 6, 'ongoing', '2017-05-11 08:00:00', '2017-05-09 09:30:00', NULL),
+(6, 7, 11, 5, 'pending', '2017-06-02 13:00:00', NULL, NULL),
+(7, 7, 11, 5, 'done', '2017-05-07 14:00:00', '2017-05-07 15:30:00', '2017-05-07 18:51:00');
 
 -- --------------------------------------------------------
 
@@ -103,7 +107,7 @@ INSERT INTO `customer` (`custId`, `firstName`, `lastName`, `address`, `email`, `
 
 CREATE TABLE `feedback` (
   `feedbackId` int(11) NOT NULL,
-  `bookingId` int(11) NOT NULL,
+  `transactionId` int(11) NOT NULL,
   `custId` int(11) NOT NULL,
   `spId` int(11) NOT NULL,
   `rating` enum('1','2','3','4','5') NOT NULL,
@@ -114,7 +118,7 @@ CREATE TABLE `feedback` (
 -- Dumping data for table `feedback`
 --
 
-INSERT INTO `feedback` (`feedbackId`, `bookingId`, `custId`, `spId`, `rating`, `remarks`) VALUES
+INSERT INTO `feedback` (`feedbackId`, `transactionId`, `custId`, `spId`, `rating`, `remarks`) VALUES
 (1, 3, 7, 11, '5', NULL);
 
 -- --------------------------------------------------------
@@ -139,6 +143,26 @@ CREATE TABLE `message` (
 
 INSERT INTO `message` (`messageId`, `senderId`, `receiverId`, `messageBody`, `timeSent`, `timeReceived`, `timeRead`) VALUES
 (1, 7, 11, 'I would like to inquire more specifically on the services you can provide', '2017-05-08 06:00:00', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `paymentdetails`
+--
+
+CREATE TABLE `paymentdetails` (
+  `invoiceId` int(11) NOT NULL,
+  `transactionId` int(11) NOT NULL,
+  `serviceMade` mediumtext NOT NULL,
+  `amount` decimal(8,3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `paymentdetails`
+--
+
+INSERT INTO `paymentdetails` (`invoiceId`, `transactionId`, `serviceMade`, `amount`) VALUES
+(1, 3, 'Painted three rooms', '200.000');
 
 -- --------------------------------------------------------
 
@@ -179,7 +203,6 @@ CREATE TABLE `service_provider` (
   `shift_start` time NOT NULL,
   `shift_end` time NOT NULL,
   `working_days` varchar(27) NOT NULL,
-  `rating` int(11) DEFAULT NULL,
   `availability` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -187,9 +210,9 @@ CREATE TABLE `service_provider` (
 -- Dumping data for table `service_provider`
 --
 
-INSERT INTO `service_provider` (`spId`, `firstName`, `lastName`, `email`, `contactNumber`, `shift_start`, `shift_end`, `working_days`, `rating`, `availability`) VALUES
-(11, 'Zebedee', 'Jimenez', 'email10@slu.edu.ph', '10', '07:30:00', '18:00:00', 'Sun,Mon,Tue,Wed,Thu,Fri,Sat', 1, 1),
-(12, 'Johnny', 'Sins', 'email12@slu.edu.ph', '11', '09:00:00', '20:00:00', 'Mon,Wed,Fri', 5, 1);
+INSERT INTO `service_provider` (`spId`, `firstName`, `lastName`, `email`, `contactNumber`, `shift_start`, `shift_end`, `working_days`, `availability`) VALUES
+(11, 'Zebedee', 'Jimenez', 'email10@slu.edu.ph', '10', '07:30:00', '18:00:00', 'Sun,Mon,Tue,Wed,Thu,Fri,Sat', 1),
+(12, 'Johnny', 'Sins', 'email12@slu.edu.ph', '11', '09:00:00', '20:00:00', 'Mon,Wed,Fri', 1);
 
 -- --------------------------------------------------------
 
@@ -225,19 +248,18 @@ INSERT INTO `sp_service` (`serviceId`, `spId`) VALUES
 
 CREATE TABLE `transaction` (
   `transactionId` int(11) NOT NULL,
-  `custId` int(11) NOT NULL,
-  `spId` int(11) NOT NULL,
   `bookingId` int(11) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `specification` text NOT NULL
+  `spStatus` enum('pending','approved') NOT NULL DEFAULT 'pending',
+  `custStatus` enum('pending','approved') NOT NULL DEFAULT 'pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `transaction`
 --
 
-INSERT INTO `transaction` (`transactionId`, `custId`, `spId`, `bookingId`, `timestamp`, `specification`) VALUES
-(2, 7, 11, 3, '2017-05-08 13:46:04', 'some services, 200');
+INSERT INTO `transaction` (`transactionId`, `bookingId`, `timestamp`, `spStatus`, `custStatus`) VALUES
+(3, 7, '2017-05-07 09:50:00', 'pending', 'pending');
 
 -- --------------------------------------------------------
 
@@ -308,7 +330,7 @@ ALTER TABLE `feedback`
   ADD PRIMARY KEY (`feedbackId`),
   ADD KEY `custId` (`custId`),
   ADD KEY `spId` (`spId`),
-  ADD KEY `bookingId` (`bookingId`);
+  ADD KEY `transactionId` (`transactionId`);
 
 --
 -- Indexes for table `message`
@@ -317,6 +339,13 @@ ALTER TABLE `message`
   ADD PRIMARY KEY (`messageId`),
   ADD KEY `senderId` (`senderId`),
   ADD KEY `receiverId` (`receiverId`);
+
+--
+-- Indexes for table `paymentdetails`
+--
+ALTER TABLE `paymentdetails`
+  ADD PRIMARY KEY (`invoiceId`),
+  ADD KEY `transactionId` (`transactionId`);
 
 --
 -- Indexes for table `service`
@@ -345,9 +374,7 @@ ALTER TABLE `sp_service`
 --
 ALTER TABLE `transaction`
   ADD PRIMARY KEY (`transactionId`),
-  ADD KEY `transaction_ibfk_1` (`bookingId`),
-  ADD KEY `custId` (`custId`),
-  ADD KEY `spId` (`spId`);
+  ADD KEY `transaction_ibfk_1` (`bookingId`);
 
 --
 -- Indexes for table `user`
@@ -364,7 +391,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `booking`
 --
 ALTER TABLE `booking`
-  MODIFY `bookingId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `bookingId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `feedback`
 --
@@ -376,6 +403,11 @@ ALTER TABLE `feedback`
 ALTER TABLE `message`
   MODIFY `messageId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
+-- AUTO_INCREMENT for table `paymentdetails`
+--
+ALTER TABLE `paymentdetails`
+  MODIFY `invoiceId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT for table `service`
 --
 ALTER TABLE `service`
@@ -384,7 +416,7 @@ ALTER TABLE `service`
 -- AUTO_INCREMENT for table `transaction`
 --
 ALTER TABLE `transaction`
-  MODIFY `transactionId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `transactionId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `user`
 --
@@ -420,7 +452,7 @@ ALTER TABLE `customer`
 ALTER TABLE `feedback`
   ADD CONSTRAINT `feedback_ibfk_1` FOREIGN KEY (`custId`) REFERENCES `customer` (`custId`),
   ADD CONSTRAINT `feedback_ibfk_2` FOREIGN KEY (`spId`) REFERENCES `service_provider` (`spId`),
-  ADD CONSTRAINT `feedback_ibfk_3` FOREIGN KEY (`bookingId`) REFERENCES `booking` (`bookingId`);
+  ADD CONSTRAINT `feedback_ibfk_3` FOREIGN KEY (`transactionId`) REFERENCES `transaction` (`transactionId`);
 
 --
 -- Constraints for table `message`
@@ -428,6 +460,12 @@ ALTER TABLE `feedback`
 ALTER TABLE `message`
   ADD CONSTRAINT `msg_rec_fk` FOREIGN KEY (`receiverId`) REFERENCES `user` (`idNum`) ON UPDATE CASCADE,
   ADD CONSTRAINT `msg_sender_fk` FOREIGN KEY (`senderId`) REFERENCES `user` (`idNum`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `paymentdetails`
+--
+ALTER TABLE `paymentdetails`
+  ADD CONSTRAINT `paymentdetails_ibfk_1` FOREIGN KEY (`transactionId`) REFERENCES `transaction` (`transactionId`);
 
 --
 -- Constraints for table `service_provider`
@@ -446,9 +484,7 @@ ALTER TABLE `sp_service`
 -- Constraints for table `transaction`
 --
 ALTER TABLE `transaction`
-  ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`bookingId`) REFERENCES `booking` (`bookingId`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `transaction_ibfk_2` FOREIGN KEY (`custId`) REFERENCES `customer` (`custId`),
-  ADD CONSTRAINT `transaction_ibfk_3` FOREIGN KEY (`spId`) REFERENCES `service_provider` (`spId`);
+  ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`bookingId`) REFERENCES `booking` (`bookingId`) ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
