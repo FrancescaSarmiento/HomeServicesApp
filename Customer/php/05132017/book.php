3eb11d81987aa25ebcaf363cbf14fce6c8444a64
Jason Dadula <2152142@slu.edu.ph>
2017-05-14 14:43:21 +0800

    minor fixes and additions
    updated book.php and spservice.php

--- Customer/php/05132017/book.php
+++ Customer/php/05132017/book.php
@@ -1,44 +1,47 @@
 <?php
 require '../includes/blockedPages.php';
 require '../includes/functions.php';
-include 'main.php';
-echo "Booking details <br><br>";
 
-$bookdate=$_GET['bookdate'];
-$spname=$_GET['sp'];
-$service=$_GET['service'];
-$spId=$_GET['spid'];
-echo "Booking date: $bookdate <br>";
+if(!empty(filter_input(INPUT_POST, 'spid'))){
+    
+$bookD=filter_input(INPUT_POST, 'bookdate');
+$spname= filter_input(INPUT_POST, 'sp');
+$serviceT= filter_input(INPUT_POST, 'type');
+$sId=filter_input(INPUT_POST, 'spid');
+$userID = filter_input(INPUT_POST, 'userID');
+echo "Booking date: $bookD <br>";
 echo "Service Provider: $spname <br>";
-echo "Service: $service <br>";?>
+echo "Service: $serviceT <br>";
+echo <<<frag
+    <form action="../includes/booksuccessful.php" method="post">
+        <input type="hidden" name="user" value="$userID">
+        <input type="hidden" name="bookD" value="$bookD">
+        <input type="hidden" name="name" value="$spname">
+        <input type="hidden" name="sType" value="$serviceT">
+        <input type="hidden" name="spId" value="$sId">
+        <button class="btn-primary" type="submit" name="confirm">Confirm</button>
+    </form>
+        <a href="?page=services&type=$serviceT" type="button">Cancel</a>
+frag;
 
-
-<html>
-<body>
-    <form action="" method="post">
-        <button type="submit" name="confirm">Confirm</button></form>
-        <a href="spservice.php?type=<?php echo $service ?>" type="button">Cancel</a>
-</body>
-</html>
-
-<?php
     #insertinto($bookdate, $spname, $service, $spId, $userID, $conn);
    
     #function insertinto($bookdate, $spname, $service, $spId, $userID, $conn){
-   if(isset( $_POST['confirm'])){
-       $validBooking = checkBookingIfValid($service, $userID, $spId, $bookdate);
-        
-        if ($validBooking == true){
-            insertBooking($service, $userID, $spId, $bookdate, $conn);
-            header ('Location: booksuccessful.php');
-        } else {
-            header ('Location: bookfail.php');
+ /*  if(!empty(filter_input(INPUT_POST, 'bookD'))){
+        $book = filter_input(INPUT_POST, 'bookD');
+        $name = filter_input(INPUT_POST, 'name');
+        $type = filter_input(INPUT_POST, 'sType');
+        $id = filter_input(INPUT_POST, 'spId');
+        if(insertBooking($type, $userID, $id,$book, $conn)){
+            echo "<script>alert(Booking request was sent to the service provider. You will be notified once the service provider responded to your request. Thank you.)</script>";
+            echo "<script>window.assign.location('main.php?page=service')</script>";
         }
+        
+    } else {
+        echo "";
     }
-
-    if(isset($_POST['cancel'])){
-       header('Location: spservice.php?type='.$service.'' );
-    }
+  * 
+  */
 /*
 function insertbooking($date, $spname, $service, $userid, $spId, $conn){
     $serviceid = "SELECT serviceId from service where servicetype=$service";
@@ -47,5 +50,5 @@ echo $date;
 
     $result = mysqli_query($conn, $insertquery) or die();
 }*/
-?>
+}
 
--- Customer/php/05132017/spservice.php
+++ Customer/php/05132017/spservice.php
@@ -1,38 +1,68 @@
-<html>
-    <head>   
-        <link href="../css/calendar.css" type="text/css" rel="stylesheet" />
-    </head>
-<body>
-
 <?php
 
-require '../includes/db_connect.php';
-include '../includes/calendar.php';
-
-$availday = isset($_GET['dayis'])? $_GET['dayis'] : null;
+$availday = filter_input(INPUT_POST, 'dayis');
 $dayExtract = strtotime($availday);
 $dayOfWeek = date('D', $dayExtract);
-$servtype = $_GET['type'];
-	global $conn;
-	$service = mysqli_real_escape_string($conn, $servtype);
-	$servicessp = "SELECT DISTINCT name, spId, working_days, availability from (SELECT concat(firstName,' ', lastName) as name, spId, working_days, availability from service_provider join sp_service using(spId) join service using(serviceId) where serviceType='$servtype') as sps where spId NOT IN(SELECT spId FROM `booking` join service_provider  using(spId) where reserved_date like '{$availday} %' and bookingStatus NOT IN ('done','cancelled')) AND (working_days like '%{$dayOfWeek}%' OR availability='1')";
+$servtype = filter_input(INPUT_POST, 'type');
+$user = $_SESSION['userID'];
+
+if(!empty($servtype)){
+        require_once '../includes/calendar.php';
+        $servtype = filter_input(INPUT_POST, 'type');
+	$servicessp = "SELECT DISTINCT name, spId, working_days from (SELECT concat(firstName,' ', lastName) as name, spId, working_days from service_provider join sp_service using(spId) join service using(serviceId) where serviceType='$servtype') as sps where spId NOT IN(SELECT spId FROM `booking` join service_provider  using(spId) where reserved_date like '{$availday} %' and bookingStatus NOT IN ('done', 'cancelled', 'rejected')) AND (working_days like '%{$dayOfWeek}%')";
 	$result = mysqli_query($conn, $servicessp);
-
-echo "Choose a date from the calendar to show available service providers.";
-$calendar = new Calendar();
-echo $calendar->show();
-
-if (mysqli_num_rows($result)>0 && $availday != null){ ?>
-	<h2> Available service providers for <span style="color:red"><?php echo $servtype ?></span> service </h2>
-    <?php
-    while($row = $result->fetch_assoc()){
-            $sp=$row["name"];
-            $spid=$row["spId"];
-			echo '<br> <a href="spprofile.php?spid='.$spid.'&type='.$servtype.'">' . $row["name"] . '</a>&nbsp <a href="book.php?bookdate='.$availday.'&sp='.$sp.'&service='.$servtype.'&spid='.$spid.'">Book Now </a><br>';
-    } 
-} else if ($availday == null){echo "";
-} else {
-            echo "Sorry, no available service provider for this date.<br> Please choose another date.";
+        if(empty(filter_input(INPUT_GET, 'year'))){
+            echo "Choose a date from the calendar to show the available service providers.";
+            $calendar = new Calendar();
+            echo $calendar->show();
+        } else {
+            echo "Choose a date from the calendar to show the available service providers.";
+            $calendar = new Calendar();
+            echo $calendar->show();
         }
+}
 
-?>
+if ($availday >= date("Y-m-d") || $availday == null){
+    if (mysqli_num_rows($result)>0 && $availday != null){ ?>
+
+	<h2> Available service providers for <span style="color:red"><?php echo $servtype ?></span> service </h2>
+    <h2> Date: <?php echo $availday ?></h2>
+
+        <?php
+        while($row = $result->fetch_assoc()){
+                $sp=$row["name"];
+                $spid=$row["spId"];
+                echo <<<frag
+                                <br>
+                                <form action="" method="post">
+                                    <input type="hidden" name="spId" value="$spid">
+                                    <input type="hidden" name="type" value="$servtype">
+                                    <input class="btn-link" type="submit" name="spinfo" value='{$row['name']}'>
+                                </form>
+                                <form method="post">
+                                    <input type="hidden" name="userID" value="$user">
+                                    <input type="hidden" name="bookdate" value="$availday">
+                                    <input type="hidden" name="sp" value="$sp">
+                                    <input type="hidden" name="type" value="$servtype">
+                                    <input type="hidden" name="spid" value="$spid">
+                                    <input class="btn-link" type="submit" name="book" value="Request Booking Schedule">
+                                </form>
+frag;
+ //<a href="?page=services&bookdate='.$availday.'&sp='.$sp.'&type='.$servtype.'&spid='.$spid.'">Book Now </a><br>';
+        } 
+    } else if ($availday == null){
+        echo "";
+    } else {
+        echo "Sorry, there are no available service provider for this date.<br> Please choose another date.";
+    }
+
+    require 'book.php';
+
+    if(!empty(filter_input(INPUT_POST, 'spId'))){
+        $spId = filter_input(INPUT_POST, 'spId');
+        $type = filter_input(INPUT_POST, 'type');
+        getUserProfile($spId, $conn, $type);
+    }
+} else {
+    echo "Sorry. You cannot book past dates. Please choose another date.";
+}
