5f3cf8b2c1defa0b871d3330f9d1321b3c381367
FrancescaSarmiento <2155825@slu.edu.ph>
2017-05-14 17:40:41 -0700

    services.php
    fixed listing of services

--- Customer/pages/services.php
+++ Customer/pages/services.php
@@ -8,14 +8,15 @@ $result = mysqli_query($conn, $query);
 		<h1><strong> Services </strong></h1>
 	
 <?php
-
+        echo "<table>";
+        echo "<tr style='background-color:#f0e044'>";
         while($row = mysqli_fetch_assoc($result)){
 	       foreach($row as $key => $value){
                     echo <<<frag
-                        <form method="post" action="">
+                        <td style='width:16%'><form method="post" action="">
                             <input type="hidden" name="type" value="$value">
                             <input class="btn btn-link" type="submit" name="submit" value="$value">
-                        </form>
+                        </form></td>
 frag;
    //<a href='?page=services&type=$value'> $value </a><br>";
                   break;
